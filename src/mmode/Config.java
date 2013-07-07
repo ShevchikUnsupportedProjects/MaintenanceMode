94b2025de55518e68029968084ca44b24e5473e8
Shevchik <Shev4ik.den@gmail.com>
2013-07-07 16:21:33 +0400

    Add configuration

--- src/mmode/Config.java
+++ src/mmode/Config.java
@@ -1,6 +1,12 @@
 package mmode;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashSet;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 
 public class Config {
 
@@ -12,13 +18,31 @@ public class Config {
 	
 	public void loadConfig()
 	{
+		FileConfiguration config = YamlConfiguration.loadConfiguration(new File("plugins/MMode/config.yml"));
+		
+		mmodeMessage = config.getString("PingMessage", mmodeMessage);
+		mmodeMOTD = config.getString("MOTD", mmodeMOTD);
+		mmodeKickMessage = config.getString("KickMessage", mmodeKickMessage);
+		mmodeAdminsList = new HashSet<String>(config.getStringList("AllowedList"));
 		
 		saveConfig();
 	}
 	
 	private void saveConfig()
 	{
+		FileConfiguration config = new YamlConfiguration();
 		
+		config.set("PingMessage", mmodeMessage);
+		config.set("MOTD", mmodeMOTD);
+		config.set("KickMessage", mmodeKickMessage);
+		config.set("AllowedList", new ArrayList<String>(mmodeAdminsList));
+		
+		try {
+			config.save(new File("plugins/MMode/config.yml"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 }
